c5ac17a124cc96d89c1c95cf88ba61f7f1e2f181
runze26 <n>
2019-10-08 19:22:30 +0800

    upgrade lodash

---
+++ src/homepage.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const HomePage = () => {
+    return (
+        <div className="homepage">
+            <div className="directory-menu">
+                <div className="menu-item">
+                    <div className="content">
+                        <h1 className="title">HATS</h1>
+                        <span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
